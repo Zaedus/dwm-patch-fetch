1b5e8b96b0fb9e46824e3fa0f69defde18d4b567
Zaedus <nintendozaedus@gmail.com>
2022-03-22 23:55:51 -0700

    Added a bunch of cool options and a readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# dwm Patch Fetch
+
+This is just a simple script which fetches all of the patches from [https://dwm.suckless.org/patches/](https://dwm.suckless.org/patches/) and parses each page for a description and a patch name.
+
+It is recommended that you save the output of this script to a file and parse it from there since every diff and description must be individually requested and parsed, making the script slow and unncessarily spam the suckless servers. There is a built in threaded mode and delay option which should be used with discretion. My intention is not to DDoS suckless, so do set the delay option to something other than 0 please!
+
+## Features
+
+- Listing the URLS of all of the diffs
+- Listing all of the patches
+
+## Why make this?
+
+Good question. The main reason I made this was becuase I was curious which version of dwm was more widely supported, and which patches did in fact support it. Using my script and the handy math function from my favorite shell `fish` I found that *70.2% of patches have explicit support for 6.2*.
+
+```fish
+math (./suckless-patches.sh | grep "6.2" | wc -l)/(./suckless-patches.sh -l | wc -l)
+```
+
+The reason I say 'explicit' is because some patches aren't named `mycoolpatch-6.2.diff` so its hard to really know an exact number through scripting, but we can safely say that over 70% of patches work with 6.2. 
+
+If you're curious about what percentage of patches explicitly support 6.3, you should download the script and find out! I can't do all the work.
+
--- suckless-patches.sh
+++ suckless-patches.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+## Variables
+
+IS_THREADED=0
+DELAY=0.5
+LIST_PATCHES=0
 PATCH_URI="https://dwm.suckless.org/patches"
 
-## Get a list of patches
-PATCHES=( $(curl --silent "$PATCH_URI/" |
-            grep "//dwm.suckless.org/patches/" |
-            grep -v "<b>patches/</b>" |
-            grep -oP '(?<=<li><a href="//dwm.suckless.org/patches/).*(?=/">)'
-        ) )
+## Functions
 
-for patch in "${PATCHES[@]}"
-do
+usage()
+{
+    echo "Usage: suckless-patches.sh [options...]"
+    echo " -t  Spawn every patch request in a separate thread."
+    echo " -d  Only in threaded mode. The delay between the creation of a new thread. (Default: 1)"
+    echo " -v  Version information."
+    echo " -h  Displays this help dialog."
+}
+
+parse()
+{
+    patch=$1
     PATCH_HTML="$(curl --silent "$PATCH_URI/$patch/")"
     DESCRIPTION="$(echo "$PATCH_HTML" | sed -n '/<h2>Description<\/h2>/,/<h2>Download<\/h2>/p' | sed '$d' | sed '1,1d' | hxselect -c -s '\n' "p")"
 
@@ -26,10 +36,76 @@ do
 
     for diff in "${DIFFS[@]}"
     do
-        if [ "${diff##*.}" == "diff" ]; then
-            chromium "$PATCH_URI/$patch/$diff"
+        if [ "${diff##*.}" == "diff" ] && ! echo "$diff" | grep "/"; then
+            echo "$PATCH_URI/$patch/$diff"
         fi
     done
 
+}
+
+## Option Parsing
+
+while getopts ":vthld:" opt; do
+    case "${opt}" in
+        t)
+            IS_THREADED=1
+            ;;
+        d)
+            re='^[0-9]+([.][0-9]+)?$' 
+            if [[ $OPTARG =~ $re ]]; then
+                IS_THREADED=1
+                DELAY="$OPTARG"
+            else
+                echo "Error: '$OPTARG' is not a positive number."
+                exit
+            fi
+            ;;
+        l)
+            LIST_PATCHES=1
+            ;;
+        v)
+            echo "suckless-patches.sh 1.0.0"
+            exit
+            ;;
+
+        h)
+            usage
+            exit
+            ;;
+        \?)
+            echo "Error: unknown option '$OPTARG'."
+            usage
+            exit
+            ;;
+        :)
+            echo "Error: option '$OPTARG' requires an argument."
+            exit
+    esac
+done
+
+## Main
+
+PATCHES=( $(curl --silent "$PATCH_URI/" |
+            grep "//dwm.suckless.org/patches/" |
+            grep -v "<b>patches/</b>" |
+            grep -oP '(?<=<li><a href="//dwm.suckless.org/patches/).*(?=/">)'
+        ) )
+
+if [ "$LIST_PATCHES" -eq 1 ]; then
+    for patch in "${PATCHES[@]}"
+    do
+        echo "$patch"
+    done
+    exit
+fi
+
+for patch in "${PATCHES[@]}"
+do
+    if [ "$IS_THREADED" -eq 1 ]; then
+        parse $patch &
+        sleep $DELAY
+    else
+        parse $patch
+    fi
 done
  
